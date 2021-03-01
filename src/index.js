6959eca3ad6fa8b797bd776d04807dc229e00611
Nikolay Golovin <nik.golovin83@gmail.com>
2021-03-01 10:08:37 +0600

    подключён redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {getFilmById} from './mocks/films.js';
 import comments from './mocks/review';
+import {reducer} from './store/reducer';
 
 
 const filmById = getFilmById;
+const store = createStore(reducer);
 
-ReactDOM.render(<App filmById = {filmById} comments = {comments}/>, document.querySelector(`#root`));
+ReactDOM.render(
+    <Provider store={store}>
+      <App filmById = {filmById} comments = {comments}/>, </Provider>,
+    document.querySelector(`#root`)
+);
