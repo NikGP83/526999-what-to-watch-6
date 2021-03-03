59796a9c2b69d0cf95dc2dcb998a16da049d2728
Nikolay Golovin <nik.golovin83@gmail.com>
2021-03-03 10:22:00 +0600

    убрано лишнее moviePageDetails

--- src/components/movie-page/tabs.jsx
+++ src/components/movie-page/tabs.jsx
@@ -3,16 +3,20 @@ import MoviePageReviews from './movie-page-reviews';
 
 const Tabs = ({children}) => {
   const [activeTab, setTab] = useState(null);
+  const movieNavListHandler = (evt, newValue) => {
+    console.log(evt.className)
+  }
+
   return (
     <nav className="movie-nav movie-card__nav">
       <ul className="movie-nav__list">
-        <li className="movie-nav__item">
+        <li onClick={movieNavListHandler}  className="movie-nav__item">
           <a href="#" className="movie-nav__link">Overview</a>
         </li>
-        <li className="movie-nav__item movie-nav__item--active">
+        <li onClick={movieNavListHandler} className="movie-nav__item movie-nav__item--active">
           <a href="#" className="movie-nav__link">Details</a>
         </li>
-        <li className="movie-nav__item">
+        <li onClick={movieNavListHandler} className="movie-nav__item">
           <a href="#" className="movie-nav__link">Reviews</a>
         </li>
       </ul>
