f2f2a5003a50e4ddb5654cc1f0a8807c028b7682
Nikolay Golovin <nik.golovin83@gmail.com>
2021-03-29 21:25:43 +0600

    компонент header закончен

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {useCurrentUser} from './use-current-user';
+import { Link } from 'react-router-dom';
+import { useCurrentUser } from './use-current-user';
 
 const Header = () => {
   const userProfile = useCurrentUser();
-  const {email} = userProfile;
+  const { email } = userProfile;
 
 
   return (
@@ -18,12 +18,19 @@ const Header = () => {
       </div>
 
       <div className="user-block">
-        <p>{email}</p>
-        <div className="user-block__avatar">
-          <Link to='/my-list'>
-            <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-          </Link>
-        </div>
+        {typeof userProfile !== `undefined` ?
+          <>
+            <p>{email}</p>
+            <div className="user-block__avatar">
+              <Link to='/my-list'>
+                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+              </Link>
+            </div>
+          </>
+          : <div className="user-block">
+            <Link to='/login' className="user-block__link">Sign in</Link>
+          </div>
+        }
       </div>
     </header>
   );
